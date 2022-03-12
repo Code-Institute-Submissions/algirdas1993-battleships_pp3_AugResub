457e6618e13077b1fa61b583f56a5902c0db054c
algirdas1993 <algirdas.baliutis@gmail.com>
2022-03-12 17:16:04 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+COMPUTER_BOARD = [[' '] * 8 for x in range(8)]
+USER_BOARD = [[' '] * 8 for x in range(8)]
+
