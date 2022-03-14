4056460364af80d1a40912e99fe1d23d27429fa5
algirdas1993 <algirdas.baliutis@gmail.com>
2022-03-14 17:36:03 +0000

    Add print statments for order

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ board = [
 ]
 
 # We want to columns by letter, but because Python accesses list by
-# numbers, dictionary needs to be defined, to translate letters to 
+# numbers, dictionary needs to be defined, to translate letters to
 # numbers.
 letters_to_numbers = {
     'A': 0,
@@ -25,7 +25,10 @@ letters_to_numbers = {
     'H': 7,
 }
 
-print("WELCOME TO ULTIME BATTLESHIPS!!!")
+print("----------------------------------")
+print("WELCOME TO ULTIMATE BATTLESHIPS!!!")
+print("----------------------------------")
+
 
 # This fuctions lets game host choose positions of the battleships.
 def ask_board_position():
@@ -40,25 +43,29 @@ def ask_board_position():
         row = input("Row (1 to 8):")
 
     # Code calling this function will receive values listed in return
-    # statment.  
+    # statment.
     return int(row) - 1, letters_to_numbers[column]
+
 
 def print_game_board(board):
     print("   A B C D E F G H")
     print("  ------------------")
     row_number = 1
+
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         print("  ------------------")
         row_number = row_number + 1
 
-# We want to have five battleships on our board, so for loop will ask 
+
+# We want to have five battleships on our board, so for loop will ask
 # for ship five times.
 
 for n in range(5):
 
     print("Where do you want your ship number:", n + 1, "?")
     row_number, column_number = ask_board_position()
+
 # This part checks if there are no repeats in the host input.
     if board[row_number][column_number] == 'X':
         print("This spot already has battleship located!")
@@ -66,7 +73,7 @@ for n in range(5):
     board[row_number][column_number] = 'X'
     print_game_board(board)
 
-# This cleanes terminal screen, and the player can start guessing 
+# This cleanes terminal screen, and the player can start guessing
 # where game host placed his battleships.
 print("\n"*40)
 print("All battleships were deployed!")
@@ -82,8 +89,8 @@ player_board = [
 ]
 
 # we will keep playing until 5 right guesses
-guess = 0
-while guess < 5:
+GUESS = 0
+while GUESS < 5:
     print("Guess battleship location on board")
     row_number, column_number = ask_board_position()
 
@@ -94,10 +101,12 @@ while guess < 5:
     if board[row_number][column_number] == 'X':
         print("You have successfully hitted the target!")
         player_board[row_number][column_number] = 'X'
-        guess = guess + 1
+        GUESS = GUESS + 1
     else:
         player_board[row_number][column_number] = '/'
         print("You have missed the target!")
 
     print_game_board(player_board)
+
 print("GAME OVER! all battleships were destroyed")
+print("-----------------------------------------")
